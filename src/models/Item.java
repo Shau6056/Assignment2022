f4124645ad2cf0d9403946da7f1d292aa27a5906
Shauna Ryan <14557997@wit.mail.com>
2022-10-19 22:56:41 +0100

    newNEw

---
+++ src/models/Item.java
@@ -0,0 +1,52 @@
+package models;
+
+import utils.Utilities;
+
+public class Item {
+
+    private String itemDescription = "No Description";
+    private boolean isItemCompleted = false;
+
+    public Item(String itemDescriptionIn)
+    {
+        itemDescription = utils.Utilities.truncateString(itemDescriptionIn, 50);
+
+    }
+
+    public Item(String itemDescriptionIn, boolean isItemCompletedIn)
+    {
+        itemDescription = utils.Utilities.truncateString(itemDescriptionIn, 50);
+
+        isItemCompleted = isItemCompletedIn;
+
+    }
+
+    public String getItemDescrption() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescriptionption) {
+        this.itemDescription = itemDescription;
+    }
+
+    public boolean isItemCompleted() {
+        return isItemCompleted;
+    }
+
+    public void setItemCompleted(boolean itemCompleted) {
+        isItemCompleted = itemCompleted;
+    }
+
+    //public boolean equals(Item itemIn)
+    //{
+        //return 0;
+//    }
+
+   // public String toString()
+    //{
+      //      return ""
+
+    //}
+
+}
+
---
+++ src/models/Note.java
@@ -0,0 +1,13 @@
+import java.util.ArrayLists;
+package models;
+
+public class Note {
+
+    private String noteTitle = "No Title";
+    private int notePriorty = 1;
+    private String noteCategory = "";
+    private boolean isNoteArchived = false;
+
+
+
+}
